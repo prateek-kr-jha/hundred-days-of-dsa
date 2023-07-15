068f156706e449b9c22f67307550a598da7c4995
Prateek <prateekj1211@gmail.com>
2023-07-15 19:22:08 +0530

    completed quick sort

--- dsa/sorting/Quick.java
+++ dsa/sorting/Quick.java
@@ -1,4 +1,6 @@
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdRandom;
+
 public class Quick {
     private static boolean less(Comparable num1, Comparable num2){
         return num1.compareTo(num2) < 0;
@@ -37,25 +39,59 @@ public class Quick {
         exch(array, lo, j);
         return j;
     }
+    public static void sort(Comparable[] array){
+        StdRandom.shuffle(array);
+        sort(array, 0, array.length - 1);
+    }
+
+    private static void sort(Comparable[] array, int lo, int hi){
+        if(lo >= hi){
+            return;
+        }
+
+        int j = partition(array, lo, hi);
+        sort(array, lo, j - 1);
+        sort(array, j + 1, hi);
+
+    }
+
+
+    private static void show(Comparable[] a) {
+        for(int i = 0; i  < a.length; i++){
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static boolean isSorted(Comparable[] a){
+        for(int i = 1; i < a.length; i++){
+            if(less(a[i], a[i - 1])){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     public static void main(String[] args){
         String[] input = In.readStrings();
-        // System.out.println(isSorted(input));
-        // show(input);
-        // sort(input, 0, input.length - 1);
-        // assert isSorted(input) : "not sorted";
-        // System.out.println(isSorted(input));
-        // show(input);
-        for(int i = 0; i < input.length; i++){
-            System.out.print(input[i] + "  ");
-        }
-        System.out.println();
+        System.out.println(isSorted(input));
+        show(input);
+        sort(input, 0, input.length - 1);
+        assert isSorted(input) : "not sorted";
+        System.out.println(isSorted(input));
+        show(input);
+        // for(int i = 0; i < input.length; i++){
+        //     System.out.print(input[i] + "  ");
+        // }
+        // System.out.println();
 
-        partition(input, 0, 15);
+        // partition(input, 0, 15);
 
-        for(int i = 0; i < input.length; i++){
-            System.out.print(input[i] + "  ");
-        }
-        System.out.println();
+        // for(int i = 0; i < input.length; i++){
+        //     System.out.print(input[i] + "  ");
+        // }
+        // System.out.println();
         
     }
 }
