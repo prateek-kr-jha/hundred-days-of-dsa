fc1e3d53b5cbbe67dd360ddd116996149f35282e
prateek <prateek.jha@yulu.bike>
2024-08-18 16:55:54 +0530

    implemented quickFind using uf interface

--- princeton-dsa/w01/UF.java
+++ princeton-dsa/w01/UF.java
@@ -1,9 +1,11 @@
 package UFinterface;
 
 public interface UF {
-    UF(int n);
     public void union(int p, int q);
+
     public int count();
+
     public int find(int p);
+
     public boolean connected(int p, int q);
 }
