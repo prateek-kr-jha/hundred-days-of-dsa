dbfddc6734fcc098a1890e5f9d0f23e5054db21e
prateek <prateek.jha@yulu.bike>
2024-09-08 01:53:31 +0530

    created helper class and helper function to create array from input

--- dsa/arrays/client.java
+++ dsa/arrays/client.java
@@ -6,7 +6,14 @@ public class client {
 
     public static void main(String[] args) {
         System.out.println("Hi, there " + args[0]);
-        String l = scn.nextLine();
-        System.out.println("bye " + l);
+        // String l = scn.nextLine();
+        // System.out.println("bye " + l);
+        // while (scn.hasNext()) System.out.println(scn.nextInt());
+        // helper help = new helper();
+        int[] testArray = helper.createArrayWithInput(scn.nextInt(), scn);
+
+        for (int i = 0; i < testArray.length; i++) {
+            System.out.println(testArray[i]);
+        }
     }
 }
